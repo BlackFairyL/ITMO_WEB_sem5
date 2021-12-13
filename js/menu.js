3398f21e94155ea02d2d40e0feb313e6f85a6eea
Elizaveta Yastreba <lyizawell@gmail.com>
2021-12-13 15:56:52 +0300

    фикс

--- js/menu.js
+++ js/menu.js
@@ -3,17 +3,14 @@ document.writeln('            <ul class="menu">');
 document.writeln('                <li class="menu__link"><a href="../web_lab1/index.html" class="menu__a" id="menu__a__id">Главная</a></li>');
 document.writeln('                <li class="menu__link"><a href="../web_lab1/about_me.html" class="menu__a" id="menu__a__id">Обо мне</a></li>');
 document.writeln('                <li class="menu__link"><a href="../web_lab1/contacts.html" class="menu__a" id="menu__a__id">Контакты</a></li>');
-document.writeln('                <li class="menu__link"><a href="../web_lab1/table.html" class="menu__a" id="menu__a__id">Таблица</a></li>');
 document.writeln('            </ul>');
 document.writeln('        </nav>');
 
-$(function () {
-    $('.menu__a').each(function () {
-        let location = window.location.href;
-        let link = this.href;
-        let result = location.match(link);
-        if(result != null) {
-            $(this).addClass('active');
+window.addEventListener('load', function() {
+    let menu = document.getElementsByClassName("menu__a");
+    for (let i = 0; i < menu.length; i++) {
+        if (menu[i].href === window.location.href) {
+            menu[i].classList.add("active");
         }
-    });
+    }
 });
